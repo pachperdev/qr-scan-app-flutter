5d5c989dd9791b00c163c179b9f8251b49c16b2b
pachperdev <pachperdev@gmail.com>
2023-02-02 11:02:57 -0500

    Leer un código QR

--- lib/widgets/scan_button.dart
+++ lib/widgets/scan_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScanButton extends StatelessWidget {
   const ScanButton({super.key});
@@ -6,7 +7,13 @@ class ScanButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      onPressed: () {},
+      onPressed: () async {
+        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+            '#3D8BEF', 'Cancelar', false, ScanMode.QR);
+
+        // ignore: avoid_print
+        print(barcodeScanRes);
+      },
       child: const Icon(Icons.filter_center_focus),
     );
   }
